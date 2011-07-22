4bbba3f2f2c0b7925b1206487458f0e40b493996
Tom Aldcroft <taldcroft@gmail.com>
2011-07-21 20:44:50 -0400

    Basic panel layout and params table

--- gui_fit.py
+++ gui_fit.py
@@ -7,6 +7,7 @@ import time
 import pygtk
 pygtk.require('2.0')
 import gtk
+from itertools import count
 
 import re
 import json
@@ -57,6 +58,7 @@ class CalcStat(object):
         self.model = model
         self.cache_fit_stat = {}
         self.min_fit_stat = None
+        self.min_par_vals = self.model.parvals
         
     def __call__(self, _data, _model, staterror=None, syserror=None, weight=None):
         """Calculate fit statistic for the xija model.  The args _data and _model
@@ -93,7 +95,8 @@ class CalcStat(object):
 
 
 class FitWorker(object):
-    def __init__(self, freeze_pars, thaw_pars):
+    def __init__(self, model, freeze_pars, thaw_pars):
+        self.model = model
         self.parent_pipe, self.child_pipe = multiprocessing.Pipe()
 
     def start(self, *args):
@@ -124,7 +127,7 @@ class FitWorker(object):
         Return a list of the thawed (fitted) parameters.
         """
         fit_parnames = set()
-        for parname, parval in zip(model.parnames, model.parvals):
+        for parname, parval in zip(self.model.parnames, self.model.parvals):
             getattr(xijamod, parname).val = parval
             fit_parnames.add(parname)
             if any([re.match(x + '$', parname) for x in freeze_pars]):
@@ -152,12 +155,12 @@ class FitWorker(object):
         ui.set_method(method)
         ui.get_method().config.update(config or sherpa_configs.get(method, {}))
 
-        xijamod = CalcModel(model)
+        xijamod = CalcModel(self.model)
         ui.load_user_model(xijamod, 'xijamod')
-        ui.add_user_pars('xijamod', model.parnames)
+        ui.add_user_pars('xijamod', self.model.parnames)
         ui.set_model(1, 'xijamod')
 
-        calc_stat = CalcStat(model, self.child_pipe)
+        calc_stat = CalcStat(self.model, self.child_pipe)
         ui.load_user_stat('xijastat', calc_stat, lambda x: np.ones_like(x))
         ui.set_stat(xijastat)
 
@@ -169,7 +172,7 @@ class FitWorker(object):
             except FitTerminated as e:
                 logging.debug('Got FitTerminated exception {}'.format(e))
                 
-        model.calc()
+        self.model.calc()
 
 
 # class HBox(gtk.HBox):
@@ -194,75 +197,153 @@ class FitWorker(object):
 #         return super(VBox, self).pack_start(child, expand, fill, padding)
         
 
+class TableColumn(dict):
+    def __init__(self, table, name, col):
+        self.table = table
+        self.name = name
+        self.col = col
+        self.table.attach(gtk.Label(name.title()), self.col, self.col + 1, 0, 1)
+        dict.__init__(self)
+
+    def __setitem__(self, row, widget):
+        if row in self:
+            self.table.remove(self[row])
+        dict.__setitem__(self, row, widget)
+        self.table.attach(widget, self.col, self.col + 1, row + 1, row + 2, xoptions=gtk.SHRINK)
+        widget.show()
+
+    def clear(self):
+        for widget in self.values():
+            self.table.remove(widget)
+        dict.clear(self)
+
+
+class WidgetTable(dict):
+    def __init__(self, n_rows, n_cols=None, colnames=None, show_header=False):
+        if n_cols is None and colnames is None:
+            raise ValueError('WidgetTable needs either n_cols or colnames')
+        if colnames:
+            self.colnames = colnames
+            self.n_cols = len(colnames)
+        else:
+            self.n_cols = n_cols
+            self.colnames = ['col{}'.format(i+1) for i in range(n_cols)]
+        self.n_rows = n_rows
+        self.show_header = show_header
+
+        self.table = gtk.Table(rows=self.n_rows, columns=self.n_cols)
+
+        self.box = gtk.ScrolledWindow()
+        self.box.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.box.add_with_viewport(self.table)
+
+        dict.__init__(self)
+            
+    def __getitem__(self, rowcol):
+        """Get widget at location (row, col) where ``col`` can be specified as
+        either a numeric index or the column name.
+
+        [Could do a nicer API that mimics np.array access, column wise by name,
+        row-wise by since numeric index, or element by (row, col).  But maybe
+        this isn't needed now].
+        """
+        row, col = rowcol
+        if col in self.colnames:
+            col = self.colnames.index(col)
+        return dict.__getitem__(self, (row, col))
+
+    def __setitem__(self, rowcol, widget):
+        row, col = rowcol
+        if rowcol in self:
+            self.table.remove(self[rowcol])
+        dict.__setitem__(self, rowcol, widget)
+        if self.show_header:
+            row += 1
+        self.table.attach(widget, col, col + 1, row, row + 1)
+        widget.show()
+
+
 class Panel(object):
     def __init__(self, orient='h', homogeneous=False, spacing=0):
         Box = gtk.HBox if orient == 'h' else gtk.VBox
         self.box = Box(homogeneous, spacing)
         self.orient = orient
 
-    def pack_start(self, child, expand=False, fill=False, padding=0):
+    def pack_start(self, child, expand=True, fill=True, padding=0):
         if isinstance(child, Panel):
             child = child.box
         return self.box.pack_start(child, expand, fill, padding)
         
-    def pack_end(self, child, expand=False, fill=False, padding=0):
+    def pack_end(self, child, expand=True, fill=True, padding=0):
         if isinstance(child, Panel):
             child = child.box
         return self.pack_start(child, expand, fill, padding)
         
 
 class PlotsPanel(Panel):
-    def __init__(self):
+    def __init__(self, fit_worker):
         Panel.__init__(self, orient='v')
+        self.fit_worker = fit_worker
         self.pack_start(gtk.Label('plots_panel'))
 
 
 class ParamsPanel(Panel):
-    def __init__(self):
+    def __init__(self, fit_worker):
         Panel.__init__(self, orient='v')
-        self.pack_start(gtk.Label('params_panel'))
+        self.fit_worker = fit_worker
+        model = fit_worker.model
+        params_table = WidgetTable(n_rows=len(model.parvals),
+                                   n_cols=2,
+                                   colnames=['parname', 'parval'],
+                                   show_header=True)
+        for row, parname, parval in zip(count(), model.parnames, model.parvals):
+            params_table[row, 0] = gtk.Label(parname)
+            params_table[row, 0].set_alignment(0, 0.5)
+            params_table[row, 1] = gtk.Label(str(parval))
+            params_table[row, 1].set_alignment(0, 0.5)
+        self.pack_start(params_table.box, padding=0)        
 
 
 class ConsolePanel(Panel):
-    def __init__(self):
+    def __init__(self, fit_worker):
         Panel.__init__(self, orient='v')
-        self.pack_start(gtk.Label('console_panel'))
+        self.fit_worker = fit_worker
+        self.pack_start(gtk.Label('console_panel'), False, False, 0)
 
 
 class ControlButtonsPanel(Panel):
-    def __init__(self):
+    def __init__(self, fit_worker):
         Panel.__init__(self, orient='h')
-        self.button = gtk.Button("Fit now")
-        self.button2 = gtk.Button("Stop!")
-        self.pack_start(gtk.Label('control_buttons_panel'))
-        self.pack_start(self.button)
-        self.pack_start(self.button2)
+        self.fit_worker = fit_worker
+        self.fit_button = gtk.Button("Fit")
+        self.stop_button = gtk.Button("Stop")
+        self.pack_start(self.fit_button, False, False, 0)
+        self.pack_start(self.stop_button, False, False, 0)
 
 
 class MainLeftPanel(Panel):
-    def __init__(self):
+    def __init__(self, fit_worker):
         Panel.__init__(self, orient='v')
-        self.control_buttons_panel = ControlButtonsPanel()
-        self.plots_panel = PlotsPanel()
-
-        self.pack_start(self.control_buttons_panel)
-        self.pack_start(self.plots_panel)
+        self.control_buttons_panel = ControlButtonsPanel(fit_worker)
+        self.plots_panel = PlotsPanel(fit_worker)
+        self.pack_start(self.control_buttons_panel, False, False, 0)
+        self.pack_start(self.plots_panel, False, False, 0)
 
 
 class MainRightPanel(Panel):
-    def __init__(self):
+    def __init__(self, fit_worker):
         Panel.__init__(self, orient='v')
-        self.params_panel = ParamsPanel()
-        self.console_panel = ConsolePanel()
+        self.params_panel = ParamsPanel(fit_worker)
+        self.console_panel = ConsolePanel(fit_worker)
 
         self.pack_start(self.params_panel)
-        self.pack_start(self.console_panel)
+        self.pack_start(self.console_panel, False, False, 0)
 
 
 class MainWindow(object):
     # This is a callback function. The data arguments are ignored
     # in this example. More on callbacks below.
-    def __init__(self):
+    def __init__(self, fit_worker):
         # create a new window
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("destroy", self.destroy)
@@ -273,14 +354,14 @@ class MainWindow(object):
         self.main_box = Panel(orient='h')
         self.window.add(self.main_box.box)
 
-        main_left_panel = MainLeftPanel()
-        main_right_panel = MainRightPanel()
-        self.main_box.pack_start(main_left_panel, expand=True, fill=True)
-        self.main_box.pack_start(main_right_panel, expand=True)
+        main_left_panel = MainLeftPanel(fit_worker)
+        main_right_panel = MainRightPanel(fit_worker)
+        self.main_box.pack_start(main_left_panel)
+        self.main_box.pack_start(main_right_panel)
 
         bp = main_left_panel.control_buttons_panel
-        bp.button.connect("clicked", fit_worker.start)
-        bp.button2.connect("clicked", fit_worker.terminate)
+        bp.fit_button.connect("clicked", fit_worker.start)
+        bp.stop_button.connect("clicked", fit_worker.terminate)
 
         # and the window
         self.window.show_all()
@@ -314,7 +395,7 @@ def get_options():
     parser = OptionParser()
     parser.add_option("--days",
                       type='float',
-                      default=30,
+                      default=15,
                       help="Number of days in fit interval (default=90")
     parser.add_option("--stop",
                       default="2010:180",
@@ -375,13 +456,11 @@ src['model'] = opt.model
 src['outdir'] = opt.outdir
 src['pardir'] = opt.pardir or opt.model
 
-if 1:
-    model_spec = json.load(open(files['model_spec.json'].abs, 'r'))
-    model = xija.ThermalModel(opt.model, start, stop, model_spec=model_spec)
-    model.make()   
-
-    model.outdir = src['outdir'].val
-    model.pardir = src['pardir'].val
+model_spec = json.load(open(files['model_spec.json'].abs, 'r'))
+model = xija.ThermalModel(opt.model, start, stop, model_spec=model_spec)
+model.make()   
+model.outdir = src['outdir'].val
+model.pardir = src['pardir'].val
 
 # Default configurations for fit methods
 sherpa_configs = dict(
@@ -398,7 +477,7 @@ freeze_pars = ('.*',
                '.*__tau_sc',
                '.*__p_ampl',
                )
-fit_worker = FitWorker(freeze_pars, thaw_pars)
+fit_worker = FitWorker(model, freeze_pars, thaw_pars)
 
 # make_out_dir()
 
@@ -415,5 +494,5 @@ fit_logger.info('Options: {0}'.format(opt))
 fit_logger.info('')
 fit_logger.info('Start time: {0}'.format(time.ctime()))
 
-main_window = MainWindow()
+main_window = MainWindow(fit_worker)
 main_window.main()
