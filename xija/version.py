8cf9306e02c6e78bf74626c4fef54eea8b5944ac
Tom Aldcroft <taldcroft@gmail.com>
2011-12-19 15:07:40 -0500

    Version 0.1

--- xija/version.py
+++ xija/version.py
@@ -8,7 +8,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.1dev'
+version = '0.1'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
