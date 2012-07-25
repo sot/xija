2acb704d329aecd826610cba42cc7dadbdf4671c
Tom Aldcroft <taldcroft@gmail.com>
2012-07-25 12:30:32 -0400

    Update version to 0.2.4

--- xija/version.py
+++ xija/version.py
@@ -8,7 +8,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.2.3'
+version = '0.2.4'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
