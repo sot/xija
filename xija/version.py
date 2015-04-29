1b58df4e52141c5f698dcb31079d289fd9d499eb
Tom Aldcroft <taldcroft@gmail.com>
2015-04-29 18:09:41 -0400

    Update version to 0.4

--- xija/version.py
+++ xija/version.py
@@ -8,7 +8,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.3.5dev'
+version = '0.4'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
