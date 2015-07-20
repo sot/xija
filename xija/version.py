bdd5160a47881bd41ce8f7007f20add8b5b63f8f
Tom Aldcroft <taldcroft@gmail.com>
2015-07-20 13:08:37 -0400

    Update version to 0.5

--- xija/version.py
+++ xija/version.py
@@ -8,7 +8,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.4'
+version = '0.5'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
