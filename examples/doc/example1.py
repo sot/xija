334368de342983eeb6a29edea98770f6c2f21263
Tom Aldcroft <taldcroft@gmail.com>
2016-01-13 18:10:08 -0500

    Add tutorial examples

---
+++ examples/doc/example1.py
@@ -0,0 +1,28 @@
+"""
+Example with a single node (ACA CCD temperature) with solar heating
+(2 bins).
+"""
+
+import xija
+
+name = __file__[:-3]
+
+model = xija.XijaModel(name, start='2015:001', stop='2015:050')
+
+model.add(xija.Node, 'aacccdpt')
+
+model.add(xija.Pitch)
+
+model.add(xija.Eclipse)
+
+model.add(xija.SolarHeat,
+          node='aacccdpt',
+          pitch_comp='pitch',
+          eclipse_comp='eclipse',
+          P_pitches=[45, 180],
+          Ps=[0.0, 0.0],
+          ampl=0.0,
+          epoch='2010:001',
+         )
+
+model.write('{}.json'.format(name))
