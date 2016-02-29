7d8efe9daf7e16369e708a3470b013132f02a960
Tom Aldcroft <taldcroft@gmail.com>
2016-02-29 15:36:35 -0500

    Update version to 0.8

--- xija/version.py
+++ xija/version.py
@@ -8,7 +8,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.7'
+version = '0.8'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
