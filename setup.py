97fd56e2a03d3b3f0ddf6f04783f95f6db086b04
Jean Connelly <jconnelly@cfa.harvard.edu>
2020-02-26 21:33:23 -0500

    Split up link args

--- setup.py
+++ setup.py
@@ -16,9 +16,11 @@ framework used Chandra thermal modeling:
 """
 
 if os.name == "nt":
-    link_args = ['/EXPORT:calc_model_1', '/EXPORT:calc_model_2']
+    core_1_link_args = ['/EXPORT:calc_model_1']
+    core_2_link_args = ['/EXPORT:calc_model_2']
 else:
-    link_args = []
+    core_1_link_args = []
+    core_2_link_args = []
 
 try:
     from testr.setup_helper import cmdclass
@@ -26,9 +28,9 @@ except ImportError:
     cmdclass = {}
 
 core1_ext = Extension('xija.core_1', ['xija/core_1.c'],
-                      extra_link_args=link_args)
+                      extra_link_args=core_1_link_args)
 core2_ext = Extension('xija.core_2', ['xija/core_2.c'],
-                      extra_link_args=link_args)
+                      extra_link_args=core_2_link_args)
 
 entry_points = {'console_scripts': 'xija_gui_fit = xija.gui_fit:main'}
 
