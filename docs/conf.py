ea88e09985cdeb45f7648300f2ca409b182710e6
Javier Gonzalez <javier.gonzalez@cfa.harvard.edu>
2020-09-22 17:19:37 -0400

    added link to tool_doc page on cxc

--- docs/conf.py
+++ docs/conf.py
@@ -110,6 +110,9 @@ html_theme_options = {
     'logotext1': 'Ska!' ,
     'logotext2': 'xija',
     'logotext3': '',
+    'homepage_url': 'https://cxc.cfa.harvard.edu/mta/ASPECT/tool_doc',
+    'homepage_text': 'ska',
+    'homepage_text_2': 'tools'
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
