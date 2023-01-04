f4a2ee0723c53f26be13fc4b087ef8cbd3a62c80
Javier Gonzalez <javierggt@yahoo.com>
2023-01-04 17:08:30 -0500

    Deploy docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.29.1',
+    VERSION: '4.19.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
